01b49598d1d9b051d317fcb52a8e0895bdc1ed2a
Tatyana <vta278@mail.ru>
2023-06-26 23:42:56 +0300

    добавлена задача1 количество четных чисел в массиве

---
+++ task5C_0/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] GenerateArray(int length, int min, int max)
+{
+    int[] array = new int [length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write(item + " ");
+    }
+}
+
+//int CountEven(int[] array)
+//{
+//int count = 0;
+//for (int i = 0; i < array.Length; i++)
+//{
+   // if(array[i]%2 == 0) 
+   // {
+   //     count++;
+   // }
+//}
+//return count;
+//}
+
+int CountEven(int[] array)
+{
+int count = 0;
+foreach (int item in array)
+{
+    if(item%2 == 0) 
+    {
+        count++;
+    }
+}
+return count;
+}
+
+int[] arr = GenerateArray(10, 100, 1000);
+ShowArray(arr);
+System.Console.WriteLine($" > {CountEven(arr)}");
+
+
