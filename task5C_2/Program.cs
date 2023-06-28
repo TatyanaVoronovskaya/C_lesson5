acf3fbd48ff24671dcbc58e9fcf8771a724ff25b
Tatyana <vta278@mail.ru>
2023-06-28 22:29:57 +0300

    добавлена задача3 массив вещественных чисел и разница Мах и Мин элемента

---
+++ task5C_2/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 3: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] GetArrayDouble()
+{
+double[] array = new double[5];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+int number = new Random().Next(1, 100);
+array[i] = rand.NextDouble() * number;
+}
+return array;
+}
+
+void ShowArray(double[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]:f2}   ");
+    }
+}
+
+double FindMax (double[] array)
+{
+    double max = array[0];
+    {
+        for (int i = 1; i < array.Length; i++)
+            {
+            if (array[i] > max)
+                {
+                max = array[i];
+                }
+            }
+        return max;
+    }
+}
+
+double FindMin (double[] array)
+{
+    double min = array[0];
+    {
+        for (int i = 1; i < array.Length; i++)
+            {
+            if (array[i] < min)
+                {
+                min = array[i];
+                }
+            }
+        return min;
+    }
+}
+
+double[] arr = GetArrayDouble();
+ShowArray(arr);
+double diff = (FindMax (arr) - FindMin (arr));
+System.Console.WriteLine($"=> {diff:f2} ");
